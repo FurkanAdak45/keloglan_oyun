16d90dcb0630d0c4bb4cf3f2c3b60eff62d3e50b
FurkanAdak45 <adakfurkanbjk@gmail.com>
2023-01-27 18:25:13 +0300

    keloglan
    keloglan zıplama oyunu

---
+++ Doodle-Jump-master/Assets/Code/KillCollider.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class KillCollider : MonoBehaviour
+{
+    public GameObject win; 
+    private GameObject finishScreen;
+
+    private void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.transform.CompareTag("Player"))
+		{
+			GameManager.Instance.GameOver();
+            win.SetActive(true);
+            Debug.Log("bitis");
+        }
+	}
+}
